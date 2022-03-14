c6df53a3f33af04f4475fd9d20e411e2196aa5aa
calmkeen <iyunseong159@gmail.com>
2022-03-14 15:16:06 +0900

    Initial Commit

---
+++ PhotosUI/PhotosUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosUIApp.swift
+//  PhotosUI
+//
+//  Created by calmkeen on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct PhotosUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
